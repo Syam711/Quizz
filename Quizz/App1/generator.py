ff3aec0d1fee1bda0e827d48e433b2ce96dcb56c
Syam711 <bashyamshyam123@gmail.com>
2025-11-11 23:24:09 +0530

    static commit

--- Quizz/App1/generator.py
+++ Quizz/App1/generator.py
@@ -3,13 +3,14 @@ import os
 from pydantic import Field, BaseModel
 
 class Problem(BaseModel):
-    question: str = Field(description="The question that will be asked based on the topic given")
+    question: str = Field(description="The question that will be asked based on the prompt given. Maintain the structure of the question in case they contain code, tables, tree maps, etc.")
     options: list[str] = Field(description="Options given to respective question")
     answer: str = Field(description="Answer to the questions which is available in given options")
     explanation: str = Field(description="Why is that option correct? Justify your answer. Keep the explanation concise.")
+    topic: str  = Field(description="Name of the topic on which the prompt is on")
 
 class ProblemSet(BaseModel):
-    problems: list[Problem] = Field(description="List of quizz questions asked on the topic and format the text according to the question. like for example if the problem is on a code then that code should have proper indentation and structure.")
+    problems: list[Problem] = Field(description="List of quizz questions asked on the topic and format the text according to the question. like for example if the problem is on a code then that code should have proper indentation and structure. The difficulty of questions should follow 2:2:1 ratio of easy:medium:hard respectively, unless the difficulty is mentioned in the prompt.")
 
 def generate(query):
     client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
--- Quizz/App1/views.py
+++ Quizz/App1/views.py
@@ -7,10 +7,8 @@ def home(request):
     if request.method=='POST':
         prompt = request.POST.get('prompt')
         global problems
-        problems = generator.generate(prompt)
-        print(len(problems))
-        global answered
-        answered = [0]*len(problems)
+        problems = generator.generate(prompt) 
+        request.session['answered'] = [0]*len(problems)
         return redirect(reverse('App1:view'))
     return render(request, 'App1/promptPage.html')
 
@@ -24,7 +22,7 @@ def view(request):
                 ind += 1
                 return render(request, 'App1/problem.html', {'problem':problems[ind], 'ind':ind})
             else:
-                return render(request, 'App1/result.html', {'correct':sum(answered), 'total':len(problems)})
+                return render(request, 'App1/result.html', {'correct':sum(request.session['answered']), 'total':len(problems)})
         elif 'prev' in request.POST:
             if ind>0:
                 ind-=1
@@ -36,11 +34,12 @@ def view(request):
             choice = int(choice)
             if problems[ind].options[choice]==problems[ind].answer:
                 res = True
-                answered[ind] = 1
+                request.session['answered'][ind] = 1
             else:
                 res = False
-                answered[ind] = 0
-            print(answered)
+                request.session['answered'][ind] = 0
+            request.session.modified = True
+            print(request.session['answered'])
             return render(request, 'App1/problem.html', {'problem':problems[ind], 'res':res, 'ind':ind, 'show':True})
     return render(request, 'App1/problem.html', {'problem':problems[0], 'ind':0})
 
